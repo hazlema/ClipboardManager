cca1f469fed72564f8c8c1f80bf2148312623fb7
Matthew Hazlett <hazlema@gmail.com>
2013-12-26 00:37:11 -0500

    First release

---
+++ ClipboardManager/Clip.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ClipboardManager
+{
+    static class Clip
+    {
+        public static void Copy(string data)
+        {
+            Clipboard.SetText(data);
+        }
+        public static void Copy(Image data)
+        {
+            Clipboard.SetImage(data);
+        }
+        public static string Paste()
+        {
+            return Clipboard.GetText();
+        }
+        public static void Clear()
+        {
+            Clipboard.Clear();
+        }
+    }
+}
---
+++ ClipboardManager/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+
+namespace ClipboardManager
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main(string[] Args)
+        {
+            Arguments CommandLine = new Arguments(Args);
+
+            if (CommandLine["cmd"] != null)
+            {
+                switch (CommandLine["cmd"])
+                {
+                    case "copy":
+                        if (CommandLine["text"] != null)
+                        {
+                            Clip.Clear();
+                            Clip.Copy(CommandLine["text"]);
+                        }
+                        else
+                        {
+                            if (CommandLine["file"] != null)
+                            {
+                                if (File.Exists(CommandLine["file"]))
+                                {
+                                    string ext = Path.GetExtension(CommandLine["file"]);
+                                    if (ext.Contains(".gif") || ext.Contains(".jpg") || ext.Contains(".png") || ext.Contains(".bmp"))
+                                    {
+                                        using (Image img = Image.FromFile(CommandLine["file"]))
+                                        {
+                                            Clip.Clear();
+                                            Clip.Copy(img);
+                                        }
+                                    } else {
+                                        Clip.Clear();
+                                        Clip.Copy(File.ReadAllText(CommandLine["file"]));
+                                    }
+                                }
+                            }
+                        }
+                        break;
+
+                    case "paste":
+                        Console.Write(Clip.Paste());
+                        break;
+
+                    case "clear": 
+                        Clip.Clear();
+                        break;
+                }
+            }
+
+            if (CommandLine["help"] != null)
+            {
+                Console.WriteLine("ClipboardManager written specificly for Brackets (http://brackets.io/)");
+                Console.WriteLine("Written by: Matthew Hazlett (hazlema@gmail.com)");
+                Console.WriteLine();
+                Console.WriteLine("Due to commandline limitations it is always preferable to copy");
+                Console.WriteLine("from a -file and not the commandline.");
+                Console.WriteLine();
+                Console.WriteLine("ClipboardManager -cmd copy -file readme.txt");
+                Console.WriteLine("ClipboardManager -cmd copy -file image.png (.gif or .bmp or .jpg)");
+                Console.WriteLine("ClipboardManager -cmd copy -text \"This is the text to copy\"");
+                Console.WriteLine("ClipboardManager -cmd paste");
+                Console.WriteLine("ClipboardManager -cmd clear");
+            }
+        }
+    }
+}
